6f93ca73d65e293453d84ef890bf1b2a2c49ad98
Fede Frederico <federicobnb27@gmail.com>
2025-01-15 17:49:22 +0100

    LUKSO UP test mint integration
    LUKSO UP test mint integration

--- src/hooks/useSmartContract.tsx
+++ src/hooks/useSmartContract.tsx
@@ -1,27 +1,40 @@
 import { useCallback } from "react";
 import { BrowserProvider, Contract, ethers, JsonRpcSigner } from "ethers";
 import lsp7Json from "../json/lsp7/lsp7.json";
-
+import { useGrid } from "../context/GridProvider";
+import { parseUnits } from "viem";
 
 export const useSmartContract = () => {
+  const { client, accounts, contextAccounts, walletConnected, setIsSearching } =
+    useGrid();
 
-    const getProvider = async (): Promise<BrowserProvider> => {
+  const getProvider = async (): Promise<BrowserProvider> => {
     if (!window.lukso) {
-        throw new Error("Lukso provider is not available on window object.");
+      throw new Error("Lukso provider is not available on window object.");
     }
     return new ethers.BrowserProvider(window.lukso);
-    };
+  };
 
-  const getSigner = useCallback(async () : Promise<JsonRpcSigner> => {
+  const getSigner = useCallback(async (): Promise<JsonRpcSigner> => {
     const browserProvider = await getProvider();
     return browserProvider.getSigner();
   }, []);
 
   const getContractInstance = useCallback(
-    async (contractAddress: string, signer: ethers.Signer) : Promise<Contract> => {
+    async (
+      contractAddress: string,
+      signer: ethers.Signer
+    ): Promise<Contract> => {
       return new ethers.Contract(contractAddress, lsp7Json.abi, signer);
-    },[]);
+    },
+    []
+  );
 
+  {
+    /**
+   this is the method to interact using ethers.js but we reccomend to use 
+  */
+  }
   const executeFunction = useCallback(
     async (contractAddress: string, functionName: string, params: any[]) => {
       try {
@@ -36,11 +49,62 @@ export const useSmartContract = () => {
         throw error;
       }
     },
-    [getSigner, getContractInstance,]
+    [getSigner, getContractInstance]
   );
+
+  const executeFunctionWithGridProvider = useCallback(async () => {
+    try {
+      if (!client || !walletConnected) {
+        console.log("Client or wallet is not connected.");
+        return;
+      }
+      const amount = 1; // Example amount for minting
+      const contractAddress = "0x86d112121996f767ab50bc9aad23c5454b72b739";
+
+      console.log("Initializing provider and signer...");
+      const provider = await getProvider();
+      const signer = await provider.getSigner();
+
+      console.log("Creating contract instance...");
+      const contract = new ethers.Contract(
+        contractAddress,
+        lsp7Json.abi,
+        signer
+      );
+
+      console.log(`Encoding data for mint function with amount: ${amount}`);
+      const data = contract.interface.encodeFunctionData("mint", [
+        contextAccounts[0], // Target address (mint recipient)
+        ethers.parseUnits(amount.toString(), "ether"), // Amount in the correct format
+      ]);
+
+      console.log("Encoded data:", data);
+
+      const tx = {
+        from: contextAccounts[0], // Sender address
+        to: contractAddress, // Contract address
+        data: data, // Function data
+        value: ethers.parseUnits(amount.toString(), "ether"), // Optional ETH value
+      };
+
+      console.log("Prepared transaction:", tx);
+
+      // Sending the transaction via the grid provider
+      console.log("Sending transaction...");
+      const txResponse = await client.sendTransaction({
+        tx,
+      });
+
+      console.log("Transaction response:", txResponse);
+    } catch (error) {
+      console.error("Transaction failed:", error);
+      throw error;
+    }
+  }, []);
 
   return {
     executeFunction,
+    executeFunctionWithGridProvider,
     getSigner,
     getContractInstance,
   };
--- src/view/HangmanGame.tsx
+++ src/view/HangmanGame.tsx
@@ -1,78 +1,82 @@
-import { useCallback, useEffect, useState } from 'react'
-import words from '../utils/ListOfWords'
-import WinLoseMessage from '../components/WinLoseMessage'
-import Hangman from '../components/Hangman'
-import Word from '../components/Word'
-import Keyboard from '../components/Keyboard'
+import { useCallback, useEffect, useState } from "react";
+import words from "../utils/ListOfWords";
+import WinLoseMessage from "../components/WinLoseMessage";
+import Hangman from "../components/Hangman";
+import Word from "../components/Word";
+import Keyboard from "../components/Keyboard";
+import { useSmartContract } from "../hooks/useSmartContract";
 
 function HangManGame() {
+  const { executeFunctionWithGridProvider } = useSmartContract(); // to test
 
-    let newWord: string = words[Math.floor(Math.random() * words.length)]
+  let newWord: string = words[Math.floor(Math.random() * words.length)];
 
-    const [ word, setWord ] = useState<string>(newWord)
+  const [word, setWord] = useState<string>(newWord);
 
-    const [ usedLetters, setUsedLetters ] = useState<string[]>([])
+  const [usedLetters, setUsedLetters] = useState<string[]>([]);
 
-    let wrongGuesses:string[] = usedLetters.filter(letter => !word.includes(letter))
+  let wrongGuesses: string[] = usedLetters.filter(
+    (letter) => !word.includes(letter)
+  );
 
-    const loser:boolean = wrongGuesses.length >= 6 
-    const winner:boolean = word.split('').every(letter => usedLetters.includes(letter))
+  const loser: boolean = wrongGuesses.length >= 6;
+  const winner: boolean = word
+    .split("")
+    .every((letter) => usedLetters.includes(letter));
 
-    const addUsedLetter = useCallback((letter: string) => {
-        if (usedLetters.includes(letter) || loser || winner) return
+  const addUsedLetter = useCallback(
+    (letter: string) => {
+      if (usedLetters.includes(letter) || loser || winner) return;
 
-        setUsedLetters(usedLetters => [ ...usedLetters, letter ])
- }, [ usedLetters ])
+      setUsedLetters((usedLetters) => [...usedLetters, letter]);
+    },
+    [usedLetters]
+  );
 
-    //event listeners for key presses
-    useEffect(() => {
-     const handleKeyPress = (ev: KeyboardEvent) => {
-         const selectedKey = ev.key
+  //event listeners for key presses
+  useEffect(() => {
+    const handleKeyPress = (ev: KeyboardEvent) => {
+      const selectedKey = ev.key;
 
-         if(!selectedKey.match(/^[a-z,A-Z]$/)) return
+      if (!selectedKey.match(/^[a-z,A-Z]$/)) return;
 
-         ev.preventDefault()
-         addUsedLetter(selectedKey.toLowerCase())
-     }
-    document.addEventListener("keypress", handleKeyPress)
-    return () => { document.removeEventListener('keypress', handleKeyPress) }
- }, [])
+      ev.preventDefault();
+      addUsedLetter(selectedKey.toLowerCase());
+    };
+    document.addEventListener("keypress", handleKeyPress);
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  }, []);
 
- function newGame(){
-     setWord(newWord)
-     setUsedLetters([])
- }
+  function newGame() {
+    setWord(newWord);
+    setUsedLetters([]);
+  }
 
- return (
-     <div
-         className='bg-white text-2xl flex flex-col items-center justify-center min-h-screen h-full w-full mx-0 '
-     >         
-            {/* win/lose message display */}
-            {winner && (
-                <WinLoseMessage  winMessage={true} newGame={newGame} />
-            )}
-            {loser && (
-                <WinLoseMessage  winMessage={false} newGame={newGame} />
-            )}
+  return (
+    <div className="bg-white text-2xl flex flex-col items-center justify-center min-h-screen h-full w-full mx-0 ">
+      {/* win/lose message display */}
+      {winner && <WinLoseMessage winMessage={true} newGame={newGame} />}
+      {loser && <WinLoseMessage winMessage={false} newGame={newGame} />}
 
-            {/* component with hangman drawing */}  
-            <Hangman numGuesses={wrongGuesses.length} />
+      {/* component with hangman drawing */}
+      <Hangman numGuesses={wrongGuesses.length} />
+      <button onClick={() => executeFunctionWithGridProvider()}>
+        executeFunctionWithGridProvider
+      </button>
 
-            {/* word to be guessed */}   
-            <Word 
-                word={word} 
-                usedLetters={usedLetters} 
-                showWord={loser}
-            />
-            
-            {/* keyboard with keys that can be selected through clicking or pressing respective key on keyboard */}
-            <Keyboard 
-                usedLetters={usedLetters} 
-                addUsedLetter={addUsedLetter} 
-                disabled={winner || loser}
-            />
-     </div>
- )
+      {/* word to be guessed */}
+      <Word word={word} usedLetters={usedLetters} showWord={loser} />
+
+      {/* keyboard with keys that can be selected through clicking or pressing respective key on keyboard */}
+      <Keyboard
+        usedLetters={usedLetters}
+        addUsedLetter={addUsedLetter}
+        disabled={winner || loser}
+      />
+    </div>
+  );
 }
 
-export default HangManGame
+export default HangManGame;
