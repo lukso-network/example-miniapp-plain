baf83c0aa012e8b213e8cefc8b9a4e3f5cebdf55
Fede Frederico <federicobnb27@gmail.com>
2025-01-17 10:01:41 +0100

    main changed
    main changed

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { GridProvider } from "./context/UpProvider.tsx";
+import { UpProvider } from "./context/UpProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GridProvider>
+    <UpProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </GridProvider>
+    </UpProvider>
   </StrictMode>
 );
